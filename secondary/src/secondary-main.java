825293c92e17e2429721986e1334382017826683
devops-agnisys <gouravgenius.agnisys@gmail.com>
2025-03-19 08:59:41 +0530

    Update secondary-main.java

--- secondary/src/secondary-main.java
+++ secondary/src/secondary-main.java
@@ -1,1 +1,5 @@
-public class secondary-main { public static void main(String[] args) { System.out.println("Hello from master-beta"); } }
+public class SecondaryMain {
+    public static void main(String[] args) {
+        System.out.println("Hello from secondary repo");
+    }
+}
